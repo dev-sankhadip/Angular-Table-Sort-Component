f519fa121a90d527f1901329d2f0c70ae00adc70
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-05-26 09:38:32 +0530

    files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SortDirective } from './directive/sort.directive';
+import { SortComponent } from './component/sort/sort.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SortDirective,
+    SortComponent
   ],
   imports: [
     BrowserModule,
