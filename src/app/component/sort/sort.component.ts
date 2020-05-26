20ec26a3c0c19a7ddc6648b79739e638b46fc8d1
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-05-26 09:37:48 +0530

    sort component added

---
+++ src/app/component/sort/sort.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sort',
+  templateUrl: './sort.component.html',
+  styleUrls: ['./sort.component.css']
+})
+export class SortComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
